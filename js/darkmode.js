9e99e1b4dbcd426582a64312ccbeb42eb3637fcb
Isabella Christensen <isch0001@stud.ek.dk>
2025-11-20 13:38:01 +0100

    darkmode

---
+++ js/darkmode.js
@@ -0,0 +1,9 @@
+const html = document.querySelector("html");
+const knap = document.querySelector("#knap");
+
+
+function toggleTheme() {
+  html.classList.toggle('dark');
+}
+
+knap.addEventListener('click', toggleTheme);
